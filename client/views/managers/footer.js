777c4dbcee36c90787bdfd874ae66f3aa76c2e6a
jayjo7 <jayjo7.hotmail.com>
2015-04-06 10:48:57 -0700

    Fax number check

--- client/views/managers/footer.js
+++ client/views/managers/footer.js
@@ -16,5 +16,26 @@ Template.footer.helpers({
 		
 		return Settings.findOne({$and : [{Key: "fax_number"}, {Value : {"$exists" : true, "$ne" : ""}}]});
 
+	},
+
+	hasFaxNumber: function(faxNumber)
+	{
+		if(faxNumber==undefined || faxNumber == null )
+		{
+			return false;
+		}
+		else
+		{
+			faxNumber = faxNumber.trim();
+			if(faxNumber.length > 0)
+			{
+				return true;
+			}
+			else
+			{
+				return false;
+			}
+		}
 	}
+
 });
